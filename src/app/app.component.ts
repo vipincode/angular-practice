85e59f31304dfa79f0d76563d096366ea199b594
Vipin Singh <svipin174@gmail.com>
2022-02-03 08:06:19 +0530

    meithod: add light and dark theme inside web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'exercise';
+export class AppComponent implements OnInit {
+
+  @HostBinding('class')
+  currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
+  constructor( private overlayContainer: OverlayContainer ) {}
+
+  ngOnInit() {
+    this.overlayContainer.getContainerElement().classList.remove('light-theme');
+    this.overlayContainer.getContainerElement().classList.add('dark-theme');
+    this.currentTheme = 'dark-theme';
+  }
+
+  setTheme({source}: MatSelectChange) {
+    if(source.value === 'light') {
+      this.currentTheme = 'light-theme';
+      this.overlayContainer.getContainerElement().classList.remove('dark-theme');
+      this.overlayContainer.getContainerElement().classList.add('light-theme');
+    } else {
+      this.overlayContainer.getContainerElement().classList.remove('light-theme');
+      this.overlayContainer.getContainerElement().classList.add('dark-theme');
+      this.currentTheme = 'dark-theme';
+    }
+  }
+
 }
